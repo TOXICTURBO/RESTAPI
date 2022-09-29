507cc436d94acbb884c6f3ee0933b791c23da683
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2022-09-29 22:32:08 +0530

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -13,13 +13,13 @@ router.get('/', (req, res) => {
     res.sendFile(__path + '/views/docs.html')
 })
 router.get('/team', (req, res) => {
-    res.sendFile(__path + '/views/team.html')
+    res.sendFile(__path + '/views/comingsoon.html')
 })
 router.get('/repo', (req, res) => {
-    res.sendFile(__path + '/views/myrepo.html')
+    res.sendFile(__path + '/views/comingsoon.html')
 })
 router.get('/contact', (req, res) => {
-    res.sendFile(__path + '/views/contact.html')
+    res.sendFile(__path + '/views/comingsoon.html')
 })
 router.get('/api/game', (req, res) => {
     res.sendFile(__path + '/views/game.html')
